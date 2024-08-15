0225b20d20229defa377ac402f06d16f60a7d0e2
toanlk <lkhactoan@gmail.com>
2024-08-15 22:30:00 +0700

    feature(*): learn nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { UserModule } from './user/user.module'
+import { PostModule } from './post/post.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({ type: 'mysql', url: process.env.DATABASE_URL }),
+    UserModule,
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
